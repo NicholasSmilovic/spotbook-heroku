2bc4c37c6bfbf9fdb1686a2a5a45b46a08155bd7
NicholasSmilovic <nicholas_smilovic@hotmail.com>
2017-10-31 23:34:02 +0000

    databaseurl added

--- knexfile.js
+++ knexfile.js
@@ -24,13 +24,8 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: {
-      key: "postgres://wvzdljcdvvjmtq:1ad24d4c12a06844eacf29e8c0d9dd8da95d8d68b6411d0ac4767f97de9380cc@ec2-54-235-250-15.compute-1.amazonaws.com:5432/dem5agrako7063",
-      host: process.env.DB_HOST,
-      database: process.env.DB_NAME,
-      user:     process.env.DB_USER,
-      password: process.env.DB_PASS
-    },
+    ssl: true,
+    connection: DB_URL,
     pool: {
       min: 2,
       max: 10
