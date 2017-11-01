93fcd9a8d9d6b0e60ef9eb2885bfdfe4f84a2f21
NicholasSmilovic <nicholas_smilovic@hotmail.com>
2017-11-01 14:06:11 +0000

    the freshest

--- app.js
+++ app.js
@@ -82,7 +82,8 @@ const uuidv1 = require('uuid/v1');
 const messageParse = require('./active-server/messageParse.js')
 const db = require('./active-server/ActivePlaylistsDB.js')
 
-const server = express()
+const wsServer = express()
+const server = https.createServer(wsServer)
 .use((req, res) => res.sendFile(INDEX) )
 .listen(8080, () => console.log(`Listening on ${ PORT }`));
 
