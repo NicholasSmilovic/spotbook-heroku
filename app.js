0ee1fc1c2a948089e01a4a140a97b21520852d41
NicholasSmilovic <nicholas_smilovic@hotmail.com>
2017-11-01 14:11:31 +0000

    the freshest

--- app.js
+++ app.js
@@ -81,10 +81,10 @@ const SocketServer = require('ws').Server;
 const uuidv1 = require('uuid/v1');
 const messageParse = require('./active-server/messageParse.js')
 const db = require('./active-server/ActivePlaylistsDB.js')
-const http = require('http');
+const https = require('https');
 
 const wsServer = express()
-const server = http.createServer(wsServer)
+const server = https.createServer(wsServer)
 .use((req, res) => res.sendFile(INDEX) )
 .listen(8080, () => console.log(`Listening on ${ PORT }`));
 
